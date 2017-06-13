e38dcc5c7e3ec768c1df54d186b3849727eadd61
Emmanuel KWENE <njume48@gmail.com>
2017-06-13 01:22:55 +0100

    Changing the feature name

---
+++ src/AdminMenu/AdminMenu.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WordPruss\AdminMenu;
+
+class AdminMenu
+{
+    protected $pageTitle;
+    protected $menuTitle;
+    protected $capability;
+    protected $menuSlug;
+    protected $function;
+    protected $iconUrl;
+    protected $position;
+
+    public function __construct() {
+    }
+}
